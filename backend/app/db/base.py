be0a9f96b626381437cf8cea803c8df1a8506646
Nathaniel Goldammer <79249678+ndgoldammer@users.noreply.github.com>
2025-11-30 18:20:40 -0500

    Revert "Merge pull request #6 from ndgldmr/feature/messages"
    This reverts commit 490dee5daa35fcad01a5a14e93cec6cc62247cc9, reversing
    changes made to 7be6c7aeac192dce80b7cb62d9a6ae3430e5162d.

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -8,7 +8,6 @@ from app.db.base_class import Base  # noqa: F401
 # Import all models here
 from app.models.user import User  # noqa: F401
 from app.models.student import Student  # noqa: F401
-from app.models.message import Message  # noqa: F401
 
 # Add new models here as they are created
 # from app.models.volunteer import Volunteer  # noqa: F401
